b5c7909b678080907ec6bffc5eaf2af0ceaae1c4
Isabella Bejarano Lopez <ibejaranol@eafit.edu.co>
2026-03-04 10:11:05 -0500

    agregue el login en la parte grafica y su arquitectura

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views  # 👈 agregar esto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Ruta del login
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
 ]
