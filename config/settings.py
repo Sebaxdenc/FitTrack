25d1993d29fafa42ba3d53154fde08827fbdee79
HardlinePizza21 <smadrido@eafit.edu.co>
2026-03-11 10:54:12 -0500

    base de datos local

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'fittrack'),
-        'USER': os.getenv('POSTGRES_USER', 'fittrack'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'fittrack'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
